a51f2573fa0aa9b295a653eaea20427ea4422287
Anakha2829 <anakhaprathapan2829@gmail.com>
2024-10-05 15:37:56 +0530

    Products Listed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'; 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
+import Products from './components/Products';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Header />} />
+        <Route path="/" element={<Products />} />
         {/* <Route path="/login" element={<LoginComponent />} /> */}
       </Routes>
     </Router>

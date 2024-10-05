44ea505866a4e7e77b7e3de57d64dfefaaf6ab2d
Neethu Prasad <neethu8psng@gmail.com>
2024-10-05 15:08:44 +0530

    done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
-const App = () => {
+function App() {
   return (
     <Router>
       <Routes>
